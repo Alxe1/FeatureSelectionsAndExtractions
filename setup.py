fe782f10e6ceaf917745e6734cc8c47cde9b668c
Alex1 <2473134393@qq.com>
2020-10-15 10:25:06 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*-coding: utf-8-*-
+
+# Author     : Littlely 
+# FileName   : setup.py
+# Description:
+from setuptools import setup, find_packages
+
+setup(
+    name="fsae",
+    version="1.0",
+    description="feature selections and feature extractions",
+    author="Littlely",
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.18.0",
+        "scikit-learn==0.20.2"
+    ]
+)
